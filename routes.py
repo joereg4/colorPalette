37ce2b1a00471e564f7c7a7ba8580ec187eef042
joereg4 <joereg4@protonmail.com>
2023-07-23 15:22:38 -0500

    Changed to Chat GPT 3.5 Turbo

--- routes.py
+++ routes.py
@@ -8,31 +8,27 @@ pages = Blueprint(
 
 
 def get_colors(msg):
-    prompt = f"""
-    You are a color palette generating assistant that responds to text prompts for color palettes
-    Your should generate color palettes that fit the theme, mood, or instructions in the prompt.
-    The palettes should be between 2 and 8 colors.
+    messages = [
+        {"role": "system",
+         "content": "You are a color palette generating assistant that responds to text prompts for color palettes.  You should generate color palettes that fit the theme, mood, or instructions in the prompt.  The palettes should be between 2 and 8 colors."},
+        {"role": "user",
+         "content": "Convert the following verbal description of a color palette into a list of colors: The Mediterranean Sea"},
+        {"role": "assistant", "content": '["#006699", "#66CCCC", "#F0E68C", "#008000", "#F08080"]'},
+        {"role": "user",
+         "content": "Convert the following verbal description of a color palette into a list of colors: sage, nature, earth"},
+        {"role": "assistant", "content": '["#EDF1D6", "#9DC08B", "#609966", "#40513B"]'},
+        {"role": "user",
+         "content": f"Convert the following verbal description of a color palette into a list of colors: {msg}"},
+    ]
 
-    Q: Convert the following verbal description of a color palette into a list of colors: The Mediterranean Sea
-    A: ["#006699", "#66CCCC", "#F0E68C", "#008000", "#F08080"]
-
-    Q: Convert the following verbal description of a color palette into a list of colors: sage, nature, earth
-    A: ["#EDF1D6", "#9DC08B", "#609966", "#40513B"]
-
-
-    Desired Format: a JSON array of hexadecimal color codes
-
-    Q: Convert the following verbal description of a color palette into a list of colors: {msg} 
-    A:
-    """
-
-    response = openai.Completion.create(
-        prompt=prompt,
-        model="text-davinci-003",
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
         max_tokens=200,
     )
 
-    colors = json.loads(response["choices"][0]["text"])
+    colors = json.loads(response["choices"][0]["message"]["content"])
+
     return colors
 
 
