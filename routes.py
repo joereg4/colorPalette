c288557ff37e3bb1b5a66eb656f96ae0e85484eb
joereg4 <joereg4@protonmail.com>
2023-09-13 20:14:08 -0500

    Changed to Chat GPT 4

--- routes.py
+++ routes.py
@@ -22,7 +22,7 @@ def get_colors(msg):
     ]
 
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4",
         messages=messages,
         max_tokens=200,
     )
